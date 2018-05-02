4ad354d450880719572b1a7a95a05a51a323e0b8
Mauro Vocale <mauro.vocale@gmail.com>
2018-05-02 16:08:54 +0200

    Fix javadoc

--- src/main/java/org/redhat/httpsession/Counter.java
+++ src/main/java/org/redhat/httpsession/Counter.java
@@ -4,39 +4,81 @@ import java.io.IOException;
 import java.io.Serializable;
 import org.jboss.logging.Logger;
 
+/**
+ * This class is a common class that increments a counter and it's store inside
+ * HTTP Session in the index.jsp presentation layer. The target is to show how
+ * the counter continue to increment its value accross the cluster.
+ *
+ * @author Mauro Vocale
+ * @version 1.0 30/04/2018
+ */
 public class Counter implements Serializable {
 
+    /**
+     * A version number for this class so that serialization can occur without
+     * worrying about the underlying class changing between serialization and
+     * deserialization.
+     */
     private static final long serialVersionUID = 6604436849415136871L;
-
-    public static final Logger logger = Logger.getLogger(Counter.class);
     
+    /**
+     * The Logger instance. All LOG messages from this class are routed through
+     * this member. The Logger namespace is the name class.
+     */
+    public static final Logger LOGGER = Logger.getLogger(Counter.class);
+    
+    /**
+     * Global counter that simulates the stateful information.
+     */
     private int counter = 0;
     
+    /**
+     * Public constructor.
+     */
     public Counter() {
-        logger.info("************************");
-        logger.info("Counter is created");
-        logger.info("************************");
+        LOGGER.info("************************");
+        LOGGER.info("Counter is created");
+        LOGGER.info("************************");
     }
-        
+    
+    /**
+     * Method that increments the global counter.
+     * 
+     * @return The updated global counter.
+     */
     public int getIncrementedValue() {
         return ++this.counter;
     }
     
-    private void writeObject(java.io.ObjectOutputStream out) 
+    /**
+     * Serialization method.
+     * 
+     * @param out ObjectOutputStream where write informations.
+     * @throws IOException 
+     */
+    private void writeObject(java.io.ObjectOutputStream out)
             throws IOException {
-        logger.info("****************************");
-        logger.info("Processing serialization");
-        logger.info("Counter = " + this.counter);
-        logger.info("****************************");
+        LOGGER.info("****************************");
+        LOGGER.info("Processing serialization");
+        LOGGER.info("Counter = " + this.counter);
+        LOGGER.info("****************************");
         out.defaultWriteObject();
     }
     
-    private void readObject(java.io.ObjectInputStream in) 
+    /**
+     * Default deserialization method
+     * 
+     * @param in ObjectInputStream from which retrieve the informations.
+     * 
+     * @throws IOException
+     * @throws ClassNotFoundException 
+     */
+    private void readObject(java.io.ObjectInputStream in)
             throws IOException, ClassNotFoundException {
         in.defaultReadObject();
-        logger.info("****************************");
-        logger.info("Processing Deserialization");
-        logger.info("Counter = " + this.counter);
-        logger.info("****************************");
+        LOGGER.info("****************************");
+        LOGGER.info("Processing Deserialization");
+        LOGGER.info("Counter = " + this.counter);
+        LOGGER.info("****************************");
     }
 }
